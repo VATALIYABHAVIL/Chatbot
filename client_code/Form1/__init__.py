02ba946079073c605d5d7f1d76e91f5e1238fb35
vataliyabhavil3@gmail.com <vataliyabhavil3@gmail.com>
2023-10-14 16:57:45 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,3 +7,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def radio_button_1_change(self, **event_args):
+    """This method is called when this radio button is selected (but not deselected)"""
+    pass
+
