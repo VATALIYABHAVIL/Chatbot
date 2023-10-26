ea021eb2044e7bc79e321381b80bdf4da95e9d41
vataliyabhavil3@gmail.com <vataliyabhavil3@gmail.com>
2023-10-26 09:40:55 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,13 +7,3 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def radio_button_1_change(self, **event_args):
-    """This method is called when this radio button is selected (but not deselected)"""
-    pass
-
-  def outlined_1_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
-    pass
-
-
